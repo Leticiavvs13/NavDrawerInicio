77d948a21ca34e79c2b6d30d5a473f4a5937bf29
Leticiavvs13 <leticiavsvieira@gmail.com>
2022-08-23 22:12:03 -0300

    Criando Toggle

--- app/src/main/java/br/com/etecia/navdrawerinicio/MainActivity.java
+++ app/src/main/java/br/com/etecia/navdrawerinicio/MainActivity.java
@@ -1,14 +1,19 @@
 package br.com.etecia.navdrawerinicio;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     MaterialToolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolBar);
 
-        toolbar.setNavigationIcon(R.drawable.ic_baseline_menu);
+        drawerLayout = findViewById(R.id.navDrawer);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "menu",Toast.LENGTH_SHORT).show();
+            }
+        });
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navopen, R.string.navclose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
